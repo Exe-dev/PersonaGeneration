1d7687e2f704d1ff212703962881c445b58dd6a0
Kai_Yoshida <14451@g.nagano-nct.ac.jp>
2021-07-24 03:47:57 +0900

    daskを使うことで処理を並列化して高速化した

--- PersonaGeneration.py
+++ PersonaGeneration.py
@@ -11,7 +11,22 @@
 # - Context: “I love running.”
 # - Response: “Me too! But only on weekends.”
 
-# In[275]:
+# {"dialog": [["没有 钱   万万 不行 ！ ~"], ["现实 就是 如此"]], "profile": [{"tag": ["漫画;旅遊;星座"], "loc": "广东 广州", "gender": "male"}, {"tag": [""], "loc": "", "gender": ""}], "uid": [0, 1]}
+
+# {'dialog': ["For what it's worth, I don't have a problem with it.", 'My apologies.  I did not have any problems with it, but I will be more careful in the future.'], 'profile': [{'tag': ["for what it's worth, i don't have a problem with it."], 'loc': '', 'gender': ''}, {'tag': [' i did not have any problems with it, but i will be more careful in the future.'], 'loc': '', 'gender': ''}], 'uid': [0, 1]}
+
+# # Constant Value
+
+# In[62]:
+
+
+NPARTITIONS = 15
+PATH = "./reddit_data/*/*.json"
+
+
+# # Imports
+
+# In[63]:
 
 
 import pandas as pd 
@@ -20,27 +35,29 @@ import bz2
 from tqdm import tqdm
 import glob
 import dask.dataframe as dd
+from dask.diagnostics import ProgressBar
 import spacy
 #import neuralcoref
 
 
-# In[276]:
+# In[64]:
 
 
 tqdm.pandas()
+ProgressBar().register()
 
 
 # # reddit_data下の全てのjsonファイルを読み込む
 
-# In[277]:
+# In[65]:
 
 
-list_bz2_file = glob.glob("./reddit_data/*/*.json")
+list_bz2_file = glob.glob(PATH)
 list_reddit_conversation = []
 list_bz2_file
 
 
-# In[278]:
+# In[66]:
 
 
 for i in range(0,len(list_bz2_file)):
@@ -50,16 +67,19 @@ for i in range(0,len(list_bz2_file)):
             list_reddit_conversation.append(dic)
 
 
-# In[279]:
+# In[67]:
 
 
 df_reddit_conversation = pd.DataFrame(list_reddit_conversation)
+df_reddit_conversation.to_csv("AllConversation.csv")
 df_reddit_conversation
 
 
-# In[280]:
-print(df_reddit_conversation.head(5))
-print(df_reddit_conversation.tail(5))
+# # 会話ペアの作成
+
+# In[68]:
+
+
 df_reddit_conversation = pd.DataFrame(list_reddit_conversation)
 df_reddit_conversation = df_reddit_conversation[df_reddit_conversation["body"]!="[deleted]"]
 df_reddit_conversation["body"] = df_reddit_conversation["body"].replace(["&lt","&gt","&amp"],["","",""])
@@ -73,13 +93,13 @@ df_reddit_conversation = df_reddit_conversation[["body","parent_body","ups","aut
 df_reddit_conversation
 
 
-# In[281]:
+# In[69]:
 
 
 nlp = spacy.load("en_core_web_sm")
 
 
-# In[282]:
+# In[70]:
 
 
 def CreatePersona(body: str):
@@ -89,7 +109,7 @@ def CreatePersona(body: str):
     return persona
 
 
-# In[283]:
+# In[71]:
 
 
 def IsPersona(sentence: str):
@@ -106,17 +126,17 @@ def IsPersona(sentence: str):
     )
 
 
-# In[284]:
+# In[73]:
 
 
-def create_json(dialog, persona0, persona1):
+def create_json(row):
     return {
-        "dialog":dialog,
+        "dialog":row["dialog"],
         "profile":[
-            {"tag":persona0,
+            {"tag":row["persona"],
             "loc":"",
             "gender":""},
-            {"tag":persona1,
+            {"tag":row["parent_persona"],
             "loc":"",
             "gender":""}
         ],
@@ -124,42 +144,54 @@ def create_json(dialog, persona0, persona1):
     }
 
 
-# In[285]:
+# # ペルソナの作成
+
+# In[74]:
 
 
-df_reddit_conversation["persona"] = df_reddit_conversation["body"].progress_apply(CreatePersona)
-df_reddit_conversation["parent_persona"] = df_reddit_conversation["parent_body"].progress_apply(CreatePersona)
+ddf_reddit_conversation = dd.from_pandas(data=df_reddit_conversation, npartitions=NPARTITIONS)
+ddf_reddit_conversation["persona"] = ddf_reddit_conversation["body"].map(CreatePersona).compute(scheduler='processes')
+ddf_reddit_conversation["parent_persona"] = ddf_reddit_conversation["parent_body"].map(CreatePersona).compute(scheduler='processes')
+#ddf_reddit_conversation = ddf_reddit_conversation[(df_reddit_conversation.astype(str)["persona"] !="[]")|(ddf_reddit_conversation.astype(str)["parent_persona"] !="[]")].reset_index(drop=True)
+#ddf_reddit_conversation["body"] = ddf_reddit_conversation["body"].map(lambda x: [x] ).compute(scheduler='processes')
+#ddf_reddit_conversation["parent_body"] = ddf_reddit_conversation["parent_body"].map(lambda x: [x] ).compute(scheduler='processes')
+df_reddit_conversation = ddf_reddit_conversation.compute()
 df_reddit_conversation = df_reddit_conversation[(df_reddit_conversation.astype(str)["persona"] !="[]")|(df_reddit_conversation.astype(str)["parent_persona"] !="[]")].reset_index(drop=True)
-df_reddit_conversation["body"] = df_reddit_conversation["body"].progress_apply(lambda x: [x] )
-df_reddit_conversation["parent_body"] = df_reddit_conversation["parent_body"].progress_apply(lambda x: [x] )
+df_reddit_conversation["body"] = df_reddit_conversation["body"].progress_map(lambda x: [x] )
+df_reddit_conversation["parent_body"] = df_reddit_conversation["parent_body"].progress_map(lambda x: [x] )
 df_reddit_conversation["dialog"] = [list(x) for x in zip(df_reddit_conversation["body"].tolist(),df_reddit_conversation["parent_body"].tolist())]
 df_reddit_conversation
 
 
-# In[286]:
+# # Json形式の作成
+
+# In[ ]:
 
 
-list_json = []
-for column_name, item in tqdm(df_reddit_conversation.iterrows()):
-    list_json.append(create_json(item["dialog"],item["persona"],item["parent_persona"]))
-df_reddit_conversation["json"] = list_json
+df_reddit_conversation["json"] = df_reddit_conversation.progress_apply(create_json, axis=1)
 df_reddit_conversation
 
 
-# In[287]:
+# # Outputs
+
+# In[ ]:
 
 
 df_reddit_conversation.to_csv("persona.csv")
 
 
-# In[288]:
+# In[ ]:
 
 
+list_json = df_reddit_conversation["json"].tolist()
 with open("created_dialogues.json", "wt", encoding="utf-8") as file:
     for dic in list_json:
         file.write(str(json.dumps(dic))+"\n")
 
 
-# {"dialog": [["没有 钱   万万 不行 ！ ~"], ["现实 就是 如此"]], "profile": [{"tag": ["漫画;旅遊;星座"], "loc": "广东 广州", "gender": "male"}, {"tag": [""], "loc": "", "gender": ""}], "uid": [0, 1]}
+# In[ ]:
 
-# {'dialog': ["For what it's worth, I don't have a problem with it.", 'My apologies.  I did not have any problems with it, but I will be more careful in the future.'], 'profile': [{'tag': ["for what it's worth, i don't have a problem with it."], 'loc': '', 'gender': ''}, {'tag': [' i did not have any problems with it, but i will be more careful in the future.'], 'loc': '', 'gender': ''}], 'uid': [0, 1]}
+
+import subprocess
+subprocess.run(['jupyter', 'nbconvert', '--to', 'script', '*.ipynb'])
+
